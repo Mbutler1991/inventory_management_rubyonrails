088ffd03e5077069d2bc19d7419e369997e6bcc3
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-02-03 15:53:47 +0000

    trying to fix deployment bugs

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module InventoryManagement
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.assets.initialize_on_precompile = false
+    config.database_url = ENV['DATABASE_URL']
 
     # Configuration for the application, engines, and railties goes here.
     #
