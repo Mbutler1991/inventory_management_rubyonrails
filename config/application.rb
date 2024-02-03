5495929bca9dc89cd79d3b44c4cab22eb476f1d2
Mbutler1991 <147847873+Mbutler1991@users.noreply.github.com>
2024-02-03 12:50:50 +0000

    fixing issues with precompiling

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module InventoryManagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
